c2d2d04507beef1644839df0089856c8079b8db4
William Warlick <wwarlick@bowdoin.edu>
2024-02-14 13:03:53 -0500

    added less less used import csv and script. Also added importID from e1 from helping behavior repo

--- scripts/cleanSona.py
+++ scripts/cleanSona.py
@@ -3,10 +3,10 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 
-filepath = 'data/e2/sona_combined.xlsx'
-filepath_to_read = 'data/e2/sona_20231218_gameData.xlsx'
+filepath = './data/e2/sona_combined.xlsx'
+filepath_to_read = './data/e2/sona_20231218_gameData.xlsx'
 
-filepath_to_write = 'data/e2/sona_20231218_gameData_cleaned.xlsx'
+filepath_to_write = './data/e2/sona_20231218_gameData_cleaned.xlsx'
 
 
 df_combinded = pd.read_excel(filepath, sheet_name='sona_combined')
@@ -16,10 +16,10 @@ first_try_gameIds = df_combinded['Attempt 1']
 df_raw = pd.read_excel(filepath_to_read, sheet_name='gameData')
 # only get rows which have gameID from first game of participants
 filtered_df = df_raw[df_raw['gameId'].isin(first_try_gameIds)]
-
-f = open(filepath_to_write, 'x')
+filtered_df = filtered_df[filtered_df['importId'].notna()]
 
 filtered_df.to_excel(filepath_to_write, index=False)
+
 
 
 
